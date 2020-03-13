064615a60e55e5a8c8380a0396e6fc39593541f9
kristoferh57 <kristoferhuang57@outlook.com>
2020-03-12 20:20:30 -0500

    Added Messages For Lance

--- Worktide/Customer Side App/TabBarController.swift
+++ Worktide/Customer Side App/TabBarController.swift
@@ -19,110 +19,73 @@ class TabBarController:UITabBarController{
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        addAuthStateChangeListener()
         setupViewControllers()
-        showWhatsNew()
+        requiredViewCheck()
         
+        NotificationCenter.default.addObserver(self, selector: #selector(self.checkViewsAgain(_:)), name: NSNotification.Name(rawValue: "showViewsListener"), object: nil)
     }
     
     func setupViewControllers(){
+        UITabBarItem.appearance().setTitleTextAttributes([NSAttributedString.Key.font: UIFont(name: "AppleSDGothicNeo-Bold", size: 10)!], for: .normal)
+        UITabBarItem.appearance().setTitleTextAttributes([NSAttributedString.Key.font: UIFont(name: "AppleSDGothicNeo-Bold", size: 10)!], for: .selected)
+        
         let mainViewController = MainViewController()
-        let appointmentsController = AppointmentsViewController()
         let notificationController = NotificationsController()
         let profileController = ProfileController(collectionViewLayout:UICollectionViewFlowLayout())
-        //add search controller in the future
         
         let mainViewNavigationController = UINavigationController(rootViewController: mainViewController)
         let notificationNavigationController = UINavigationController(rootViewController: notificationController)
-        let appointmentsNavigationController = UINavigationController(rootViewController: appointmentsController)
         let profileNavigationController = UINavigationController(rootViewController: profileController)
         
-        let mainViewTabBarItem = UITabBarItem(title: nil, image: UIImage(named: "homeIconTabBar")?.withRenderingMode(UIImage.RenderingMode.alwaysOriginal), selectedImage: UIImage(named: "homeIconTabBar"))
-        mainViewTabBarItem.imageInsets = UIEdgeInsets(top: 9, left: 0, bottom: -9, right: 0)
-
-        let appointmentsTabBarItem = UITabBarItem(title: nil, image: UIImage(named: "appointmentsIconTabBar")?.withRenderingMode(UIImage.RenderingMode.alwaysOriginal), selectedImage: UIImage(named: "appointmentsIconTabBar"))
-        appointmentsTabBarItem.imageInsets = UIEdgeInsets(top: 9, left: 0, bottom: -9, right: 0)
+        let mainViewTabBarItem = UITabBarItem(title:"Home", image: UIImage(named: "homeIconTabBar")?.withRenderingMode(UIImage.RenderingMode.alwaysOriginal), selectedImage: UIImage(named: "homeIconTabBar"))
         
-        let notificationTabBarItem = UITabBarItem(title: nil, image: UIImage(named: "notificationTabBarcon")?.withRenderingMode(UIImage.RenderingMode.alwaysOriginal), selectedImage: UIImage(named: "notificationTabBarcon"))
-        notificationTabBarItem.imageInsets = UIEdgeInsets(top: 9, left: 0, bottom: -9, right: 0)
+        let notificationTabBarItem = UITabBarItem(title: "Bookings", image: UIImage(named: "notificationTabBarcon")?.withRenderingMode(UIImage.RenderingMode.alwaysOriginal), selectedImage: UIImage(named: "notificationTabBarcon"))
         
-        let profileTabBarItem = UITabBarItem(title: nil, image: UIImage(named: "profileTabBarIcon")?.withRenderingMode(UIImage.RenderingMode.alwaysOriginal), selectedImage: UIImage(named: "profileTabBarIcon"))
-        profileTabBarItem.imageInsets = UIEdgeInsets(top: 9, left: 0, bottom: -9, right: 0)
+        let profileTabBarItem = UITabBarItem(title: "Profile", image: UIImage(named: "profileTabBarIcon")?.withRenderingMode(UIImage.RenderingMode.alwaysOriginal), selectedImage: UIImage(named: "profileTabBarIcon"))
         
         mainViewNavigationController.tabBarItem = mainViewTabBarItem
-        appointmentsNavigationController.tabBarItem = appointmentsTabBarItem
         notificationNavigationController.tabBarItem = notificationTabBarItem
         profileNavigationController.tabBarItem = profileTabBarItem
         
-        if shouldShowAppointmentsController {
-            viewControllers = [mainViewNavigationController, appointmentsNavigationController, notificationNavigationController, profileNavigationController]
-        } else {
-            viewControllers = [mainViewNavigationController, notificationNavigationController,profileNavigationController]
-        }
-        
-        viewControllers = [mainViewNavigationController, appointmentsNavigationController, notificationNavigationController, profileNavigationController]
+        viewControllers = [mainViewNavigationController, notificationNavigationController,profileNavigationController]
         
     }
     
-    func addAuthStateChangeListener(){
-        Auth.auth().addStateDidChangeListener { auth, user in
-            if(auth.currentUser == nil){
-                if(self.viewControllers?.count == 4){
-                    self.viewControllers?.remove(at: 1)
-                }
-            } else {
-                self.shouldAppointmentController()
-            }
-        }
-    }
     
-    func shouldAppointmentController(){
-        guard let userID = Auth.auth().currentUser?.uid else {
-            return
-        }
-        
-        let db = Firestore.firestore()
-        db.collection("Services").whereField("creatorID", isEqualTo: userID).addSnapshotListener { querySnapshot, error in
-            guard let documents = querySnapshot?.documents else {
-                print("Error fetching documents: \(error!)")
-                return
-            }
-            
-            if documents.count == 0 {
-                if(self.viewControllers?.count == 4){
-                    self.viewControllers?.remove(at: 1)
-                }
-            } else {
-                if(self.viewControllers?.count == 3){
-                    self.showAppointmentsController()
-                }
-                
-            }
-        }
-    }
     
-    func showAppointmentsController(){
-        let appointmentsController = AppointmentsViewController()
-        let appointmentsNavigationController = UINavigationController(rootViewController: appointmentsController)
-        let appointmentsTabBarItem = UITabBarItem(title: nil, image: UIImage(named: "appointmentsIconTabBar")?.withRenderingMode(UIImage.RenderingMode.alwaysOriginal), selectedImage: UIImage(named: "appointmentsIconTabBar"))
-        appointmentsTabBarItem.imageInsets = UIEdgeInsets(top: 9, left: 0, bottom: -9, right: 0)
+    func requiredViewCheck(){
         
-        appointmentsNavigationController.tabBarItem = appointmentsTabBarItem
-        self.viewControllers?.insert(appointmentsNavigationController, at: 1)
+        if(UserDefaults.standard.string(forKey: "WhatsNew") != "YHZMsXkH0qVu7Z"){
+            showWhatsNew()
+        } else if !checkIfWeCanSeeLocation() || (UserDefaults.standard.bool(forKey: "AskLocationPermission") != false){
+            showEnableLocationController()
+        }
     }
     
     func showWhatsNew(){
-        
-        if(UserDefaults.standard.string(forKey: "WhatsNew") != "YHZMsXkH0qVu7Z"){
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-                let viewController = WhatsNewViewController()
-                let navigationController = UINavigationController(rootViewController: viewController)
-                self.present(navigationController, animated: true)
-                //UserDefaults.standard.set("YHZMsXkH0qVu7Z", forKey: "WhatsNew")
-            }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            let viewController = WhatsNewViewController()
+            let navigationController = UINavigationController(rootViewController: viewController)
+            navigationController.modalPresentationStyle = .overFullScreen
+            
+            self.present(navigationController, animated: true)
+            //UserDefaults.standard.set("YHZMsXkH0qVu7Z", forKey: "WhatsNew")
         }
     }
     
+    func showEnableLocationController(){
+        //show enable location
+        let viewController = EnableLocationController()
+        viewController.backButton.isHidden = true
+        viewController.locationText.text = "We need your location to ensure the services you see are in your area."
+        let navigationController = UINavigationController(rootViewController: viewController)
+        navigationController.modalPresentationStyle = .overFullScreen
+        self.present(navigationController, animated: true)
+    }
+    
+    @objc func checkViewsAgain(_ notification: NSNotification) {
+           requiredViewCheck()
+    }
     
     func checkIfWeCanSeeLocation() -> Bool{
         if CLLocationManager.locationServicesEnabled() {
@@ -139,13 +102,7 @@ class TabBarController:UITabBarController{
         }
     }
     
-    func openEnableLocationController(){
-        let viewController = EnableLocationController()
-        viewController.backButton.isHidden = true
-        viewController.locationText.text = "We need your location to ensure the services you see are in your area."
-        let navigationController = UINavigationController(rootViewController: viewController)
-        navigationController.modalPresentationStyle = .overFullScreen
-        self.present(navigationController, animated: true)
-    }
+    
+
     
 }
--- Worktide/Shared Files App/WhatsNewViewController.swift
+++ Worktide/Shared Files App/WhatsNewViewController.swift
@@ -7,205 +7,93 @@
 //
 
 import UIKit
-import MapKit
 
 class WhatsNewViewController: UIViewController{
     
+    //MARK: Item Init
+    // you need to initialize items like these. IOS comes with UIKit that has these items.
+    private let backgroundView:UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false // you always need this because we are using autolayout
+        view.backgroundColor = .green //these items have properties you can change
+        return view
+    }()
+    
     private let imageView:UIImageView = {
-        let imageView = UIImageView(image: UIImage(named: "priceIcon"))
+        let imageView = UIImageView(image: UIImage(named: "paymentIcon")) // this is how u set an image in an image. or imageView.image = UIImage()
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
     
-    private let newTitle:UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "Service Description"
-        label.numberOfLines = 0
-        label.font = UIFont(name: "AppleSDGothicNeo-Bold", size: 18)
-        label.textAlignment = .left
-        label.textColor = .black
-        label.adjustsFontSizeToFitWidth = true
-        return label
-    }()
-    
-    private let newDescription:UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "Let customers know what they get for your services, navigate to the service in your profile to add the description."
-        label.numberOfLines = 0
-        label.font = UIFont(name: "AppleSDGothicNeo-Regular", size: 16)
-        label.textAlignment = .left
-        label.textColor = .darkGray
-        label.adjustsFontSizeToFitWidth = true
-        return label
-    }()
-    
-    private let imageView1:UIImageView = {
-        let imageView = UIImageView(image: UIImage(named: "imageIcon"))
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        return imageView
-    }()
-    
-    private let newTitle1:UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "Service Images"
-        label.numberOfLines = 0
-        label.font = UIFont(name: "AppleSDGothicNeo-Bold", size: 18)
-        label.textAlignment = .left
-        label.textColor = .black
-        label.adjustsFontSizeToFitWidth = true
-        return label
-    }()
-    
-    private let newDescription1:UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "Show off your work, and add images to your services."
-        label.numberOfLines = 0
-        label.font = UIFont(name: "AppleSDGothicNeo-Regular", size: 16)
-        label.textAlignment = .left
-        label.textColor = .darkGray
-        label.adjustsFontSizeToFitWidth = true
-        return label
-    }()
-    
-    private let imageView2:UIImageView = {
-        let imageView = UIImageView(image: UIImage(named: "linkIcon"))
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        return imageView
-    }()
-    
-    private let newTitle2:UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "Profile Link"
-        label.numberOfLines = 0
-        label.font = UIFont(name: "AppleSDGothicNeo-Bold", size: 18)
-        label.textAlignment = .left
-        label.textColor = .black
-        label.adjustsFontSizeToFitWidth = true
-        return label
-    }()
-    
-    private let newDescription2:UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "Share your services and let people book with you through Worktide."
-        label.numberOfLines = 0
-        label.font = UIFont(name: "AppleSDGothicNeo-Regular", size: 16)
-        label.textAlignment = .left
-        label.textColor = .darkGray
-        label.adjustsFontSizeToFitWidth = true
-        return label
-    }()
-    
-    private let doneBtn: UIButton = {
+    private let skipButton:UIButton = { // some item have different properties so you can just search that up online or look through the project
         let button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitleColor(UIColor.white, for: .normal)
-        button.setTitle("Done", for: .normal)
-        button.backgroundColor = UIColor(red: 0, green: 122/255, blue: 255/255, alpha: 1)
-        button.layer.cornerRadius = 16
-        button.layer.masksToBounds =  false
-        button.contentEdgeInsets = UIEdgeInsets(top: 10,left: 10,bottom: 10,right: 10)
-        button.layer.shadowColor = UIColor.gray.cgColor
-        button.layer.shadowOffset = CGSize.zero
-        button.layer.shadowOpacity = 0.2
-        button.layer.shadowRadius = 1.0
-        button.addTarget(self, action: #selector(doneButton), for: .touchUpInside)
-        button.contentHorizontalAlignment = .center
-        button.titleLabel?.font = UIFont(name: "AppleSDGothicNeo-Regular", size: 16)!
+        button.setTitle("Skip", for: .normal)
+        button.setTitleColor(UIColor.systemBlue, for: .normal)
+        button.addTarget(self, action: #selector(doneButton), for: .touchUpInside) //see func doneButton
         return button
     }()
     
+    //MARK: IOS Lifecycle
+    //ios has lifecycles that gets called when we call this class. this is the first that it will be called
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavigation()
         setupView()
-        
-        checkForLocationPermission()
-        
     }
     
+    //other lifecycles but we dont need for this
+    /*override func viewWillAppear(_ animated: Bool) {
+        <#code#>
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        <#code#>
+    }*/
+    
+    //MARK: SETUP VIEWS
+    // I organize my files into functions of what I needa do
     func setupNavigation(){
-        navigationController?.setNavigationBarHidden(false, animated: true)
-        navigationController?.navigationBar.prefersLargeTitles = true
-        navigationController?.navigationBar.barTintColor = UIColor.white
-        self.navigationController?.navigationBar.shadowImage = UIImage()
-        title = "What's New"
-        
+        navigationController?.setNavigationBarHidden(true, animated: true) //used just to hide the navigation bar
     }
     
     func setupView(){
-        view.backgroundColor = .white
         
+        // UIViewController which is this class comes with a view that is bascially the same screen size
+        view.backgroundColor = .white // we need to set this to a color or else it will be transparent
+        
+        view.addSubview(backgroundView) //then we add the UI items we initialized like this
         view.addSubview(imageView)
-        imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30).isActive = true
-        imageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 190).isActive = true
+        
+        //this is to set the location and size of the views.
+        backgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true // means background view left side is aligned with the views left side
+        backgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true //backgroundview right side aligned view right side
+        backgroundView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true // background view top aligned with top of view/screen
+        backgroundView.heightAnchor.constraint(equalToConstant: 300).isActive = true // the height
+        
+        imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true//center of the view/screen
+        imageView.centerYAnchor.constraint(equalTo: backgroundView.centerYAnchor).isActive = true//center of backgroundviews center vertically
         imageView.widthAnchor.constraint(equalToConstant: 40).isActive = true
         imageView.heightAnchor.constraint(equalToConstant: 40).isActive = true
-        
-        view.addSubview(newTitle)
-        newTitle.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: 15).isActive = true
-        newTitle.topAnchor.constraint(equalTo: view.topAnchor, constant: 180).isActive = true
-        newTitle.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -25).isActive = true
-        
-        view.addSubview(newDescription)
-        newDescription.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: 15).isActive = true
-        newDescription.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30).isActive = true
-        newDescription.topAnchor.constraint(equalTo: newTitle.bottomAnchor, constant: 5).isActive = true
-        //-----------------------------------------------
-        
-        view.addSubview(imageView1)
-        imageView1.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30).isActive = true
-        imageView1.topAnchor.constraint(equalTo: newDescription.bottomAnchor, constant: 35).isActive = true
-        imageView1.widthAnchor.constraint(equalToConstant: 40).isActive = true
-        imageView1.heightAnchor.constraint(equalToConstant: 40).isActive = true
-        
-        view.addSubview(newTitle1)
-        newTitle1.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: 15).isActive = true
-        newTitle1.topAnchor.constraint(equalTo: newDescription.bottomAnchor, constant: 25).isActive = true
-        newTitle1.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -25).isActive = true
-        
-        view.addSubview(newDescription1)
-        newDescription1.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: 15).isActive = true
-        newDescription1.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30).isActive = true
-        newDescription1.topAnchor.constraint(equalTo: newTitle1.bottomAnchor, constant: 5).isActive = true
-        
-        //-----------------------------------------------
-        view.addSubview(imageView2)
-        imageView2.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30).isActive = true
-        imageView2.topAnchor.constraint(equalTo: newDescription1.bottomAnchor, constant: 35).isActive = true
-        imageView2.widthAnchor.constraint(equalToConstant: 40).isActive = true
-        imageView2.heightAnchor.constraint(equalToConstant: 40).isActive = true
-        
-        view.addSubview(newTitle2)
-        newTitle2.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: 15).isActive = true
-        newTitle2.topAnchor.constraint(equalTo: newDescription1.bottomAnchor, constant: 25).isActive = true
-        newTitle2.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -25).isActive = true
-        
-        view.addSubview(newDescription2)
-        newDescription2.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: 15).isActive = true
-        newDescription2.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30).isActive = true
-        newDescription2.topAnchor.constraint(equalTo: newTitle2.bottomAnchor, constant: 5).isActive = true
-        
-        view.addSubview(doneBtn)
-        doneBtn.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -30).isActive = true
-        doneBtn.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.85).isActive = true
-        doneBtn.heightAnchor.constraint(equalToConstant: 55).isActive = true
-        doneBtn.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
     }
     
+    //MARK: LISTENERS
+    //we use this to set when buttons are pressed
     @objc func doneButton(sender: UIButton!) {
         
+        //switch because we might add more buttons
         switch sender {
-        case doneBtn:
+        case skipButton:
             self.dismiss(animated: true, completion: {
-                self.registerForRemoteNotification()
+                // this is for me just leave it like this.
+                NotificationCenter.default.post(name: NSNotification.Name(rawValue: "showViewsListener"), object: nil, userInfo: nil)
             })
-        
+            
+        /*case nextButton:
+            let viewController = WhatsNewViewController1()
+            self.navigationController?.pushViewController(viewController, animated: true)
+        *///this is how you will show the next viewcontroller.
+            
         default:
             break
         }
@@ -213,49 +101,13 @@ class WhatsNewViewController: UIViewController{
         
     }
     
-    func checkForLocationPermission(){
-       if CLLocationManager.locationServicesEnabled() {
-        switch CLLocationManager.authorizationStatus() {
-           case .notDetermined, .restricted, .denied:
-                self.openEnableLocationController()
-           case .authorizedAlways, .authorizedWhenInUse:
-                break
-           @unknown default:
-               self.openEnableLocationController()
-           }
-       } else {
-           self.openEnableLocationController()
-       }
-   }
     
-    func openEnableLocationController(){
-        let viewController = EnableLocationController()
-        viewController.locationText.text = "Just in case your address doesn't match your location. Let us remind you."
-        let navigationController = UINavigationController(rootViewController: viewController)
-        navigationController.modalPresentationStyle = .overFullScreen
-        self.present(navigationController, animated: true)
+}
+
+//this is the next view controller you needa show im just putting it into one file just for this part so its easier. Others you would usually create a new file for a new vewicontroller.
+class WhatsNewViewController1:UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
     }
-    
-    func registerForRemoteNotification() {
-
-        if #available(iOS 10.0, *) {
-            let center  = UNUserNotificationCenter.current()
-
-            center.requestAuthorization(options: [.sound, .alert, .badge]) { (granted, error) in
-                if error == nil{
-                    DispatchQueue.main.async(execute: {
-                      UIApplication.shared.registerForRemoteNotifications()
-                    })
-                }
-            }
-         
-
-        }else {
-            UIApplication.shared.registerUserNotificationSettings(UIUserNotificationSettings(types: [.sound, .alert, .badge], categories: nil))
-            UIApplication.shared.registerForRemoteNotifications()
-        }
-        
-    }
-    
-    
 }
